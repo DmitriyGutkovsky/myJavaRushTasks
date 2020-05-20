27d471a78bbbc7fd11335318535eebac00acfbda
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-05-20 16:48:00 +0300

    task1631 (6 level  13 lecture) - completed

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task16.task1631;
+
+
+import com.javarush.task.task16.task1631.common.*;
+
+public class ImageReaderFactory {
+
+    public static ImageReader getImageReader(ImageTypes imageTypes) {
+        if (imageTypes != null) {
+            switch (imageTypes) {
+                case BMP:
+                    return new BmpReader();
+                case JPG:
+                    return new JpgReader();
+                case PNG:
+                    return new PngReader();
+                default:
+                    throw new IllegalArgumentException("Неизвестный тип картинки");
+            }
+        } else {
+            throw new IllegalArgumentException("Неизвестный тип картинки");
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/common/JpgReader.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task16.task1631.common;
+
+public class JpgReader implements ImageReader {
+
+    public JpgReader() {
+        System.out.println("JpgReader created");
+    }
+
+}
