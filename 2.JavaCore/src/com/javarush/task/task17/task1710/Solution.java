fda59ac3ad6b927305f458ce1cdf501402393a03
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-05-27 14:09:34 +0300

    task1710 (7 level  10 lecture) - completed

--- 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task17.task1710;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /* 
 CRUD
@@ -18,25 +21,21 @@ public class Solution {
 
     public static void main(String[] args) {
         //start here - начни тут
-        switch (args[0]){
+        switch (args[0]) {
             case ("-c"):
                 //-c name sex bd
-                System.out.println("c");
                 addPersonToList(args[1], args[2], args[3]);
                 break;
             case ("-u"):
                 //-u id name sex bd
-                System.out.println("u");
                 addPersonByIdToList(args[1], args[2], args[3], args[4]);
                 break;
             case ("-d"):
                 //-d id
-                System.out.println("d");
                 deletePerson(args[1]);
                 break;
             case ("-i"):
                 //-i id
-                System.out.println("i");
                 infoByPerson(args[1]);
                 break;
             default:
@@ -46,23 +45,100 @@ public class Solution {
 
     private static void addPersonToList(String name, String sexString, String bd) {
         // -c - добавляет человека с заданными параметрами в конец allPeople, выводит id (index) на экран
-        if (sexString.equals("м")) {
-            Sex sex = Sex.MALE;
-        } else if (sexString.equals("ж")) {
-            Sex sex = Sex.FEMALE;
+        //bd - дата рождения в следующем формате 15/04/1990
+        // parsing person's date of birthday
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        Date d = null;
+        try {
+            d = dateFormat.parse(bd);
+        } catch (ParseException e) {
+            System.out.println(e);
         }
 
+        // parsing person's sex and  adding person to allPeopple list
+        if (sexString.equals("м")) {
+            Person person = Person.createMale(name, d);
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+        } else if (sexString.equals("ж")) {
+            Person person = Person.createFemale(name, d);
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+        }
     }
 
-    private static void addPersonByIdToList(String id, String name, String sex, String bd) {
+    private static void addPersonByIdToList(String id, String name, String sexString, String bd) {
         // -u - обновляет данные человека с данным id
+        // parsing person's date of birthday
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        Date d = null;
+        try {
+            d = dateFormat.parse(bd);
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+
+        int index = 0;
+        try{
+            index = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+
+        // parsing person's sex and  adding person to allPeopple list
+        if (sexString.equals("м")) {
+            Person person = Person.createMale(name, d);
+            allPeople.remove(index);
+            allPeople.add(index, person);
+        } else if (sexString.equals("ж")) {
+            Person person = Person.createFemale(name, d);
+            allPeople.remove(index);
+            allPeople.add(index, person);
+        }
     }
 
     private static void deletePerson(String id) {
         // -d - производит логическое удаление человека с id, заменяет все его данные на null
+        int index = 0;
+        try{
+            index = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+
+        Person person = allPeople.get(index);
+        person.setName(null);
+        person.setBirthDate(null);
+        person.setSex(null);
+
+        allPeople.remove(index);
+        allPeople.add(index, person);
     }
 
     private static void infoByPerson(String id) {
-        // -i - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)
+        // -i - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990).
+        int index = 0;
+        try{
+            index = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+        Person person = allPeople.get(index);
+        String  printSex = null;
+        Sex sex = person.getSex();
+        if (sex.equals(Sex.MALE)) {
+            printSex = "м";
+        } else printSex = "ж";
+
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        String newBD;
+        newBD = newDateFormat.format(person.getBirthDate());
+
+        System.out.println(person.getName() + " " + printSex + " " + newBD);
+
+
+
+
+
     }
 }
---
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Test.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task17.task1710;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+class Test {
+
+    public static void main(String[] args) {
+        String t = "15/04/1990";
+//        String t = "04/15/1990";
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, new Locale("en", "FR"));
+//        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, Locale.FRENCH);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        SimpleDateFormat newFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+        Date d = null;
+        try{
+//            d = df.parse(t);
+            d = sdf.parse(t);
+            String nd = newFormat.format(d);
+            System.out.println(d);
+            System.out.println(nd);
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
+
+    }
+
+}
