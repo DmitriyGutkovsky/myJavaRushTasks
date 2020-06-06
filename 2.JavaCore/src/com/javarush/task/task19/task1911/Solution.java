9159ed4a55bb83c02a171626846aa2b213b99bc8
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-06 22:54:32 +0300

    task1911 (9 level  8 lecture) - completed

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task19.task1911;
+
+/* 
+Ридер обертка
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream out = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+
+        System.setOut(stream);
+
+        testString.printSomething();
+        String incomingText = outputStream.toString();
+
+        System.setOut(out);
+
+        String result = incomingText.toUpperCase();
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
