d02861872348f98553e46152eae570a220e8452e
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-06 16:37:35 +0300

    task1827 (8 level  11 lecture) - started

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task18.task1827;
+
+/* 
+Прайсы
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine(); // D:\Дима\JavaRush\JavaRushTasks\JavaRushTasks\2.JavaCore\src\com\javarush\task\task18\task1827\test.txt
+        
+    }
+}
