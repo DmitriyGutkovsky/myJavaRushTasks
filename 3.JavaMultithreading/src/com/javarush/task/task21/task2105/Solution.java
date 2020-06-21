d4f5d64d85136944120aeaf0b2ed8340bdfbf654
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-21 19:26:03 +0300

    task2105 (1 level 5 lecture) - completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o.getClass() != this.getClass()) return false;
+        if (!(o instanceof Solution))
+            return false;
+        if (this == o) return true;
+        return ((first == ((Solution) o).first) || (first != null && first.equals(((Solution) o).first)))
+                && ((last == ((Solution) o).last) || (last != null && last.equals(((Solution) o).last)));
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
