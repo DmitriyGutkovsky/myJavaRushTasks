021a7776e9cd8d5fa378a045ad6a3b53ded01913
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-21 20:13:03 +0300

    task2104 (1 level 5 lecture) - completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    @Override
+    public boolean equals(Object n) {
+        if (this == n) return true;
+        if (!(n instanceof Solution)) return false;
+        Solution solution1 = (Solution) n;
+        return Objects.equals(first, solution1.first) &&
+                Objects.equals(last, solution1.last);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, last);
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
