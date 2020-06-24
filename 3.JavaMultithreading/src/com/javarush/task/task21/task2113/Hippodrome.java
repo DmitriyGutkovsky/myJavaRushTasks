4eb1d55a79132aaac3181cb24b4cde4d2d595a50
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-24 22:54:12 +0300

    task2113 (1 level 16 lecture) - part 6 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses = new ArrayList<>();
+    static Hippodrome game;
 
     public List<Horse> getHorses() {
         return horses;
@@ -15,6 +16,17 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
+        game = new Hippodrome(new ArrayList<>());
+
+        Horse horse1 = new Horse("One", 3, 0);
+        Horse horse2 = new Horse("One", 3, 0);
+        Horse horse3 = new Horse("One", 3, 0);
+
+        game.horses.add(horse1);
+        game.horses.add(horse2);
+        game.horses.add(horse3);
+
+
 
 
     }
