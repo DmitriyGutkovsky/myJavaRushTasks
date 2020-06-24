bef16f37bff06bf6fb760da2572ac037ea798430
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-24 22:39:21 +0300

    task2113 (1 level 16 lecture) - part 5 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -4,4 +4,34 @@ public class Horse {
     String name;
     double speed;
     double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
 }
