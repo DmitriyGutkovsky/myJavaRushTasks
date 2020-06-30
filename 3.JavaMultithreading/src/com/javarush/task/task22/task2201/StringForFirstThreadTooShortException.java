fd7024f46f2899f299c45170fd4078bfbab7a726
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-06-30 23:14:55 +0300

    task2201 (2 level 5 lecture) - completed

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForFirstThreadTooShortException.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForFirstThreadTooShortException.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task22.task2201;
 
 public class StringForFirstThreadTooShortException extends RuntimeException {
+    public StringForFirstThreadTooShortException() {
+    }
+
+    public StringForFirstThreadTooShortException(Exception e) {
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return new StringIndexOutOfBoundsException("string index out of range: -1");
+    }
 }
