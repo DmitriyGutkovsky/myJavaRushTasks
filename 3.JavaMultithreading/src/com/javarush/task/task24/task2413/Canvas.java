ec1589b90d75ac0aaf2de009ae02ced71a107df8
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-07-29 21:37:57 +0300

    task2413 (3 level 13 lecture) - game Arkanoid - part 9 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,4 +1,37 @@
 package com.javarush.task.task24.task2413;
 
 public class Canvas {
+    private int width;
+    private  int height;
+    private char[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Canvas(int width, int heigth) {
+        this.width = width;
+        this.height = heigth;
+        matrix = new char[this.height +2][this.width+2];
+    }
 }
