6b22077905e294ab26a835ec1e5592f44a194e04
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-07-29 20:42:56 +0300

    task2413 (3 level 13 lecture) - game Arkanoid - part 2 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,25 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int width;
+    private int height;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
     public static void main(String[] args) {
 
     }
