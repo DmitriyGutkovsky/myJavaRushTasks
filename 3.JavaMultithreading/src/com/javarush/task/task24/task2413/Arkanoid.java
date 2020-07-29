3a39449897aa2052b264c52f77699d44949def25
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-07-29 20:48:36 +0300

    task2413 (3 level 13 lecture) - game Arkanoid - part 4 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     private int width;
     private int height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
 
     public void setHeight(int height) {
         this.height = height;
@@ -18,6 +23,30 @@ public class Arkanoid {
 
     public int getWidth() {
         return width;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 
     public Arkanoid(int width, int height) {
