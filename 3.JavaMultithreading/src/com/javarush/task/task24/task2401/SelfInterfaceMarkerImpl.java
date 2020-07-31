2c75087a611917cbf3a3816efbd1d1aa9a3eaa92
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-07-31 13:55:24 +0300

    task2401 (4 level 2 lecture) - completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void doSomething(){
+        System.out.println("do something");
+    }
+
+    public void printSomething(){
+        System.out.println("print something");
+    }
+}
