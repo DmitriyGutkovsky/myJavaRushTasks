c7ecbb85eda843038621753c24654a4983ae3db1
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-08-30 22:46:51 +0300

    task3008 (6 level 15 lecture) - Chat - part 8 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -4,31 +4,47 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
-    private static class Handler extends Thread{
+
+    private static class Handler extends Thread {
         private Socket socket;
-        public Handler (Socket socket){
+
+        public Handler(Socket socket) {
             this.socket = socket;
         }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            connection.send(new Message(MessageType.NAME_REQUEST));
+            Message receiveMessage = connection.receive();
+            if (receiveMessage.getType() == MessageType.USER_NAME &&
+                    !receiveMessage.getData().isEmpty() &&
+                    !connectionMap.containsKey(receiveMessage.getData())) {
+                connectionMap.put(receiveMessage.getData(), connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+            } else  return serverHandshake(connection);
+            return receiveMessage.getData();
+        }
     }
-    public static void sendBroadcastMessage(Message message){
+
+    public static void sendBroadcastMessage(Message message) {
         try {
-            for(Map.Entry<String, Connection> pair : connectionMap.entrySet()){
+            for (Map.Entry<String, Connection> pair : connectionMap.entrySet()) {
                 pair.getValue().send(message);
-            };
+            }
+            ;
 
         } catch (IOException e) {
             ConsoleHelper.writeMessage("Сообщение не было отправлено");
         }
     }
+
     public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
             ConsoleHelper.writeMessage("Сервер запущен");
-            for(;;){
+            for (; ; ) {
                 new Handler(serverSocket.accept()).start();
             }
 
