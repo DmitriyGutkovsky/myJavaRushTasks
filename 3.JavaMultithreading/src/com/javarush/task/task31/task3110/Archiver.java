4ac691145eab046d9934ce3fc3ba83d714a8b8a7
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-07 22:15:15 +0300

    task3110 (7 level 15 lecture) - Archiver - part 8 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,27 +1,49 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
+
+import java.io.IOException;
 
 public class Archiver {
+
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите полный путь архива");
-        try {
-            String pathToArchive = reader.readLine();
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathToArchive));
-            System.out.println("Введите путь к файлу, который будем архивировать");
-            String pathToFile = reader.readLine();
-            zipFileManager.createZip(Paths.get(pathToFile));
 
-            new ExitCommand().execute();
+        Operation operation = null;
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT);
 
     }
+
+    public static Operation askOperation() throws IOException {
+
+        ConsoleHelper.writeMessage("Выберите операцию:\n" +
+                Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
+                Operation.ADD.ordinal() + " - добавить файл в архив\n" +
+                Operation.REMOVE.ordinal() + " - удалить файл из архива\n" +
+                Operation.EXTRACT.ordinal() + " - распаковать архив\n" +
+                Operation.CONTENT.ordinal() + " - просмотреть содержимое архива\n" +
+                Operation.EXIT.ordinal() + " - выход");
+//            ConsoleHelper.writeMessage("Выберите операцию:\n" +
+//                    "0 - упаковать файлы в архив\n" +
+//                    "1 - добавить файл в архив\n" +
+//                    "2 - удалить файл из архива\n" +
+//                    "3 - распаковать архив\n" +
+//                    "4 - просмотреть содержимое архива\n" +
+//                    "5 - выход");
+        int numberOfOperation = ConsoleHelper.readInt();
+        Operation[] values = Operation.values();
+        return values[numberOfOperation];
+    }
+
+
 }
