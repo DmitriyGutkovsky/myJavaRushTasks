c8c8a2964a0bd2d0f1fc72c9a2e0873f9e64b15e
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-09 10:19:53 +0300

    task3110 (7 level 15 lecture) - Archiver - part 12 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -7,14 +7,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public abstract class ZipCommand implements Command {
-    @Override
-    public void execute() throws Exception {
-    }
 
-    public ZipFileManager getZipFileManager() throws Exception{
-        ConsoleHelper.writeMessage("введите полный путь файла архива");
-        String fullPath = ConsoleHelper.readString();
-        Path path = Paths.get(fullPath);
-        return new ZipFileManager(path);
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }
