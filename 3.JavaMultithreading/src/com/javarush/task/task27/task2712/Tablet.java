717f9b572f12a583d077a49b4e1845bd99e52192
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-19 23:14:57 +0300

    task2712 (9 level 15 lecture) - Restaurant menu - part 1 - completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task27.task2712;
+
+public class Tablet {
+    private final int number ; //это номер планшета, чтобы можно было однозначно установить, откуда поступил заказ
+
+    public Tablet(int number) {
+        this.number = number;
+    }
+
+    //будет создавать заказ из тех блюд, которые выберет пользователь.
+    public void createOrder(){
+
+    }
+}
