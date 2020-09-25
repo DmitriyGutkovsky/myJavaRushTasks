7bc0abf59d1c0d4003c33d74a2f5ff7370dbdb96
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-25 11:43:47 +0300

    task2712 (9 level 15 lecture) - Restaurant menu - part 13 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/statistic/StatisticManager.java
@@ -1,11 +1,15 @@
 package com.javarush.task.task27.task2712.statistic;
 
 import com.javarush.task.task27.task2712.statistic.event.EventDataRow;
+import com.javarush.task.task27.task2712.statistic.event.EventType;
+
+import java.util.*;
 
 //С его помощью будем регистрировать события в хранилище
 public class StatisticManager {
 
     private static StatisticManager instance;
+    private StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {
     }
@@ -19,4 +23,17 @@ public class StatisticManager {
 
     public void register(EventDataRow data) {
     }
+
+    private class StatisticStorage {
+
+        Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticStorage() {
+            EventType[] values = EventType.values();
+            for (EventType value : values) {
+                storage.put(value, new ArrayList<>());
+            }
+        }
+    }
 }
+
