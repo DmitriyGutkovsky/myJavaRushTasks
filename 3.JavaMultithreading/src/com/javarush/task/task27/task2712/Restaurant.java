8181763ac2bd3d1e61363a8eef6ff45e261c2cf0
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-09-30 17:56:50 +0300

    task2712 (9 level 15 lecture) - Restaurant menu - part 19 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -2,24 +2,46 @@ package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Waiter;
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private static final int ORDER_CREATING_INTERVAL = 100;
 
     public static void main(String[] args) {
-        Tablet tablet1 = new Tablet(1);
-
-        Cook cook = new Cook("Dima");
-        Waiter waiter = new Waiter();
-        tablet1.addObserver(cook);
-        cook.addObserver(waiter);
-        for (int i = 1; i < 2; i++) {
-            tablet1.createOrder();
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            tablets.add(new Tablet(i + 1));
         }
+
+        Cook cookDima = new Cook("Dima");
+        Cook cookNastya = new Cook("Nastya");
+
+        StatisticManager.getInstance().register(cookDima);
+        StatisticManager.getInstance().register(cookNastya);
+        
+        for (Tablet tablet : tablets) {
+            tablet.addObserver(cookDima);
+            tablet.addObserver(cookNastya);
+        }
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATING_INTERVAL));
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+
         DirectorTablet directorTablet = new DirectorTablet();
-        directorTablet.printActiveVideoSet();
         directorTablet.printAdvertisementProfit();
-        directorTablet.printArchivedVideoSet();
         directorTablet.printCookWorkloading();
+        directorTablet.printActiveVideoSet();
+        directorTablet.printArchivedVideoSet();
     }
 }
