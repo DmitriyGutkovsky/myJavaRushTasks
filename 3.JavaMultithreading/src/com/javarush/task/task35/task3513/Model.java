e6daa819c523d826aac11a4e7c51df113895c6f8
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-07 22:36:22 +0300

    task3513 (10 level 15 lecture) - Game 2048 - part 3 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -2,4 +2,15 @@ package com.javarush.task.task35.task3513;
 
 //будет содержать игровую логику и хранить игровое поле
 public class Model {
+    private static final int FIELD_WIDTH = 4; // определяющая ширину игрового поля
+    private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+
+    public Model() {
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles.length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+
+    }
 }
