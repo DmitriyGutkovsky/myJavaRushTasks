6a83ed64abd3762b81fd4e006c2066964ac5260a
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-07 22:05:03 +0300

    task3513 (10 level 15 lecture) - Game 2048 - part 1 - completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task35.task3513;
+
+//будет содержать только метод main и служить точкой входа в наше приложение.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
