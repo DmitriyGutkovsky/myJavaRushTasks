6e619ff729f8ff46b73cba8f2ae7d20fe9bf46c7
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-07 23:00:09 +0300

    task3513 (10 level 15 lecture) - Game 2048 - part 4 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,16 +1,46 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //будет содержать игровую логику и хранить игровое поле
 public class Model {
     private static final int FIELD_WIDTH = 4; // определяющая ширину игрового поля
     private Tile[][] gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
     public Model() {
+        resetGameTiles();
+    }
+
+    private List getEmptyTiles() {
+        List<Tile> emptyTiles = new ArrayList<>();
+        for (Tile[] gameTile : gameTiles) {
+            for (int j = 0; j < gameTiles.length; j++) {
+                if (gameTile[j].value == 0) {
+                    emptyTiles.add(gameTile[j]);
+                }
+            }
+        }
+        return emptyTiles;
+    }
+
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (!emptyTiles.isEmpty()) {
+            int index = (int) (Math.random() * emptyTiles.size()) % emptyTiles.size();
+            Tile emptyTile = emptyTiles.get(index);
+            emptyTile.value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
+
+    public void resetGameTiles() {
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles.length; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
-
+        addTile();
+        addTile();
     }
+
 }
