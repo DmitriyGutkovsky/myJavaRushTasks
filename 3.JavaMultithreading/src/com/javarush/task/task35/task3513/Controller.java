3a2b7fe62dfc00c95c500e09db67ab5549647579
DmitriyGutkovsky <dmitriy.gutkovsky@gmail.com>
2020-10-08 10:18:12 +0300

    task3513 (10 level 15 lecture) - Game 2048 - part 9 - completed

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,17 +1,63 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 //будет следить за нажатием клавиш во время игры
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private static final int WINNING_TILE = 2048;
 
-    public Tile[][] getGameTiles(){
-       return model.getGameTiles();
+    public Controller(Model model) {
+        view = new View(this);
+        this.model = model;
     }
 
-    public int getScore(){
+    public Tile[][] getGameTiles() {
+        return model.getGameTiles();
+    }
+
+    public int getScore() {
         return model.score;
     }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        }
+
+        if (model.canMove() == false) {
+            view.isGameLost = true;
+        }
+
+        if (view.isGameLost == false && view.isGameWon == false) {
+            if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                model.left();
+            }
+            if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                model.right();
+            }
+            if (e.getKeyCode() == KeyEvent.VK_UP) {
+                model.up();
+            }
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                model.down();
+            }
+
+            if (model.maxTile == WINNING_TILE) {
+                view.isGameWon = true;
+            }
+        }
+        view.repaint();
+    }
+
 }
